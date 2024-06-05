efbbfe18f6f98a4626e67646e23ebe1c8a7a0576
Akshay Ganesh <akshayiyerg@gmail.com>
2024-06-04 20:39:21 -0400

    Created required model and service for api calling.

--- MVVM & Combine Tutorial/ViewController.swift
+++ MVVM & Combine Tutorial/ViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
     
-    // repo commit test
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,30 @@ class ViewController: UIViewController {
     
     @IBAction func refreshBtnAction(_ sender: Any) {
         
+    }
+    
+    protocol QuoteServiceType {
+        func getRandomQuote() -> AnyPublisher<Quote, Error>
+    }
+    
+    class QuoteService: QuoteServiceType {
+        
+        func getRandomQuote() -> AnyPublisher<ViewController.Quote, any Error> {
+            let url = URL(string: "https://api.quotable.io/random")!
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .catch { error in
+                    return Fail(error: error).eraseToAnyPublisher()
+                }.map({ $0.data })
+                .decode(type: Quote.self, decoder: JSONDecoder())
+                .eraseToAnyPublisher()
+        }
+        
+        
+    }
+    
+    struct Quote: Decodable {
+        let content: String
+        let author: String
     }
     
 }
